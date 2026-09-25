c36facadfbe43c3bd6ab1dd1d2635a50cb0ab29b
huohuo <huohuo@users.noreply.github.com>
2026-09-25 22:43:54 +0800

    自动同步单词 2026-09-25 22:43

--- words.js
+++ words.js
@@ -2275,7 +2275,8 @@ window.WORDS = [
     "contextZh": "科幻小说",
     "category": "音乐词汇",
     "source": "小赫转存",
-    "id": 170
+    "id": 170,
+    "audioFile": "18e7501046bdbb52"
   },
   {
     "word": "Alien",
